3aee46de945b0e7a879e7b8b58d05e2cf05dd33e
Daniel Doran <ddoran3@gmail.com>
2023-07-07 17:30:22 -0600

    Dragon stuff

--- Dragon/DragonBody.gd
+++ Dragon/DragonBody.gd
@@ -1,32 +1,41 @@
 extends CharacterBody2D
 
 @export var speed: float = 500
-@export var stop_time: float = 0.2
 @export var turn_speed:float = PI
 @export_range(0, 180, 0.1, "radians") var forward_limit: float = PI/4
+@export var tank_controls = true
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	var direction = Vector2(Input.get_axis("Left", "Right"),Input.get_axis("Up", "Down")).limit_length()
 	
-	var moving = false
+	var max_turn = turn_speed * delta
 	
-	if(direction.length_squared() > 0):
-		var target_rot: float = Vector2.UP.angle_to(direction)
-		var turn: float = target_rot - rotation
-		turn = fposmod(turn + PI, 2*PI)-PI
-		var max_turn = turn_speed * delta
-		rotation += clampf(turn,-max_turn,max_turn)
-		if(abs(turn)-max_turn < forward_limit):
-			moving = true
+	var in_direction = Vector2(Input.get_axis("Left", "Right"),Input.get_axis("Up", "Down"))
 	
-	
-	if moving:
-		velocity = direction * speed
+	if tank_controls:
+		rotate(in_direction.x * max_turn)
+		
+		velocity = transform.basis_xform(Vector2.DOWN)*in_direction.y*speed
+		
 	else:
-		velocity = Vector2(0,0)
+		var direction = in_direction.limit_length()
+	
+		var moving = false
+	
+		if(direction.length_squared() > 0):
+			var target_rot: float = Vector2.UP.angle_to(direction)
+			var turn: float = target_rot - rotation
+			turn = fposmod(turn + PI, 2*PI)-PI
+			rotate(clampf(turn,-max_turn,max_turn))
+			if(abs(turn)-max_turn < forward_limit):
+				moving = true
+		
+		if moving:
+			velocity = direction * speed
+		else:
+			velocity = Vector2(0,0)
 
 	move_and_slide()
--- Dragon/DragonHead.gd
+++ Dragon/DragonHead.gd
@@ -2,9 +2,19 @@ extends Node2D
 
 signal fire(on:bool)
 
+@export var neck_length: float = 150
+
 func _physics_process(delta):
 	if(Input.is_action_just_pressed("Fire")):
 		fire.emit(true)
 	if(Input.is_action_just_released("Fire")):
 		fire.emit(false)
+		
+	var parent_trans: Transform2D = get_parent().global_transform
+		
+	var target_position = parent_trans.basis_xform_inv(get_global_mouse_position() - parent_trans.origin)
+	target_position = target_position.limit_length(150)
+	position = target_position
+	rotation = Vector2.UP.angle_to(target_position)
+	
 	
